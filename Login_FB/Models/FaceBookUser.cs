4d882603e3c79f112e56c8a4f522d5cc6b02e5c2
sagar <sagurana.sa143@gmail.com>
2019-06-26 17:02:06 +0530

    Add project files.

---
+++ Login_FB/Models/FaceBookUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FaceBook_Login_MVC.Models
+{
+    public class FaceBookUser
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string PictureUrl { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Login_FB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Login_FB.Startup))]
+namespace Login_FB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
